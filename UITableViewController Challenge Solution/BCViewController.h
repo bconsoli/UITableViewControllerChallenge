de2f413980d2525099cea98c840077d49f31aac7
Bill Consoli <billconsoli@BC-Mac-Pro.local>
2014-03-04 13:06:45 -0600

    Initial Commit

---
+++ UITableViewController Challenge Solution/BCViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Bill Consoli on 3/4/14.
+//  Copyright (c) 2014 Bill Consoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Bill Consoli on 3/4/14.
+//  Copyright (c) 2014 Bill Consoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BCAppDelegate class]));
+    }
+}
